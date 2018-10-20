fa5cdd9e76309e77c7c59e4c4ef3cf4ee10e5a0d
shibafu <shibafu528@gmail.com>
2018-10-21 02:30:07 +0900

    Add throws annotation to Retryable

--- core/src/main/java/mastodon4j/api/Retryable.kt
+++ core/src/main/java/mastodon4j/api/Retryable.kt
@@ -1,5 +1,8 @@
 package mastodon4j.api
 
+import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
+
 interface Retryable {
+    @Throws(Mastodon4jRequestException::class)
     fun retry()
 }
