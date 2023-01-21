ae833a82d17705b7e1bd4e35461d33eef50f6fc1
takke <takke30@gmail.com>
2023-01-21 19:34:40 +0900

    reformat

--- core/src/main/java/com/sys1yagi/mastodon4j/api/entity/auth/AccessToken.kt
+++ core/src/main/java/com/sys1yagi/mastodon4j/api/entity/auth/AccessToken.kt
@@ -4,15 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 
 class AccessToken(
-        @SerializedName("access_token")
-        var accessToken: String = "",
+    @SerializedName("access_token")
+    var accessToken: String = "",
 
-        @SerializedName("token_type")
-        var tokenType: String = "",
+    @SerializedName("token_type")
+    var tokenType: String = "",
 
-        @SerializedName("scope")
-        var scope: String = "",
+    @SerializedName("scope")
+    var scope: String = "",
 
-        @SerializedName("created_at")
-        var createdAt: Long = 0L) {
-}
+    @SerializedName("created_at")
+    var createdAt: Long = 0L
+)
