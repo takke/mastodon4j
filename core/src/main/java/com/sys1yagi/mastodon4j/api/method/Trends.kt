6e6286c954788aa5e7d593d9b401674428b3f2ef
takke <takke30@gmail.com>
2023-02-02 12:22:53 +0900

    add Trends

---
+++ core/src/main/java/com/sys1yagi/mastodon4j/api/method/Trends.kt
@@ -0,0 +1,37 @@
+package com.sys1yagi.mastodon4j.api.method
+
+import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.MastodonRequest
+import com.sys1yagi.mastodon4j.Parameter
+import com.sys1yagi.mastodon4j.api.entity.Tag
+import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
+
+/**
+ * see more https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#timelines
+ */
+class Trends(private val client: MastodonClient) {
+
+    // GET /api/v1/trends/tags
+    @JvmOverloads
+    @Throws(Mastodon4jRequestException::class)
+    fun getTrendsTags(limit: Int? = null, offset: Int? = null): MastodonRequest<List<Tag>> {
+        return MastodonRequest<List<Tag>>(
+            {
+                client.get(
+                    "trends/tags",
+                    Parameter().apply {
+                        limit?.let {
+                            append("limit", it)
+                        }
+                        offset?.let {
+                            append("offset", it)
+                        }
+                    }
+                )
+            },
+            {
+                client.getSerializer().fromJson(it, Tag::class.java)
+            }
+        )
+    }
+}
