0ee2c9d5c4ab8f496d230a595ea0527ea7aed126
takke <takke30@gmail.com>
2023-02-02 12:24:40 +0900

    data class Tag

--- core/src/main/java/com/sys1yagi/mastodon4j/api/entity/Tag.kt
+++ core/src/main/java/com/sys1yagi/mastodon4j/api/entity/Tag.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 /**
  * see more https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#tag
  */
-class Tag(
+data class Tag(
     @SerializedName("name")
     val name: String = "",
 
