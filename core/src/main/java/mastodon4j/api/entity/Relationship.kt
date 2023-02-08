f3ad6eb91aaabbe75ca2b155fe0cbad1c746a275
takke <takke30@gmail.com>
2023-02-08 13:23:58 +0900

    relationship

--- core/src/main/java/mastodon4j/api/entity/Relationship.kt
+++ core/src/main/java/mastodon4j/api/entity/Relationship.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 /**
  * see more https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#relationship
  */
-class Relationship(
+data class Relationship(
     @SerializedName("id") val id: Long = 0L,
     @SerializedName("following") val isFollowing: Boolean = false,
     @SerializedName("followed_by") val isFollowedBy: Boolean = false,
