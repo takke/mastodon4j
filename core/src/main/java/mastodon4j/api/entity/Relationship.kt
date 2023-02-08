e77605113cc781a1f53553f216aa46d1b9223591
takke <takke30@gmail.com>
2023-02-08 10:58:17 +0900

    reformat

--- core/src/main/java/mastodon4j/api/entity/Relationship.kt
+++ core/src/main/java/mastodon4j/api/entity/Relationship.kt
@@ -6,21 +6,10 @@ import com.google.gson.annotations.SerializedName
  * see more https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#relationship
  */
 class Relationship(
-    @SerializedName("id")
-    val id: Long = 0L,
-
-    @SerializedName("following")
-    val isFollowing: Boolean = false,
-
-    @SerializedName("followed_by")
-    val isFollowedBy: Boolean = false,
-
-    @SerializedName("blocking")
-    val isBlocking: Boolean = false,
-
-    @SerializedName("muting")
-    val isMuting: Boolean = false,
-
-    @SerializedName("requested")
-    val isRequested: Boolean = false
+    @SerializedName("id") val id: Long = 0L,
+    @SerializedName("following") val isFollowing: Boolean = false,
+    @SerializedName("followed_by") val isFollowedBy: Boolean = false,
+    @SerializedName("blocking") val isBlocking: Boolean = false,
+    @SerializedName("muting") val isMuting: Boolean = false,
+    @SerializedName("requested") val isRequested: Boolean = false
 )
