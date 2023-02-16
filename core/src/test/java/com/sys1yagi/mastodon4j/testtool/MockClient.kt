e4a8a3e25e060b218176ad0a99cbb61aa6082bd7
takke <takke30@gmail.com>
2023-02-16 19:07:13 +0900

    reformat

--- core/src/test/java/com/sys1yagi/mastodon4j/testtool/MockClient.kt
+++ core/src/test/java/com/sys1yagi/mastodon4j/testtool/MockClient.kt
@@ -25,27 +25,29 @@ object MockClient {
     fun mock(jsonName: String, maxId: Long? = null, sinceId: Long? = null): MastodonClient {
         val client: MastodonClient = mock()
         val response: Response = Response.Builder()
-                .code(200)
-                .request(Request.Builder().url("https://test.com/").build())
-                .protocol(Protocol.HTTP_1_1)
-                .body(ResponseBody.create(
+            .code(200)
+            .request(Request.Builder().url("https://test.com/").build())
+            .protocol(Protocol.HTTP_1_1)
+            .body(
+                ResponseBody.create(
                     "application/json; charset=utf-8".toMediaTypeOrNull(),
                     AssetsUtil.readFromAssets(jsonName)
-                ))
-                .apply {
-                    val linkHeader = arrayListOf<String>().apply {
-                        maxId?.let {
-                            add("""<https://mstdn.jp/api/v1/timelines/public?limit=20&local=true&max_id=$it>; rel="next"""")
-                        }
-                        sinceId?.let {
-                            add("""<https://mstdn.jp/api/v1/timelines/public?limit=20&local=true&since_id=$it>; rel="prev"""")
-                        }
-                    }.joinToString(separator = ",")
-                    if (linkHeader.isNotEmpty()) {
-                        header("link", linkHeader)
+                )
+            )
+            .apply {
+                val linkHeader = arrayListOf<String>().apply {
+                    maxId?.let {
+                        add("""<https://mstdn.jp/api/v1/timelines/public?limit=20&local=true&max_id=$it>; rel="next"""")
                     }
+                    sinceId?.let {
+                        add("""<https://mstdn.jp/api/v1/timelines/public?limit=20&local=true&since_id=$it>; rel="prev"""")
+                    }
+                }.joinToString(separator = ",")
+                if (linkHeader.isNotEmpty()) {
+                    header("link", linkHeader)
                 }
-                .build()
+            }
+            .build()
         setResponse(client, response)
         return client
     }
@@ -55,15 +57,17 @@ object MockClient {
         val source: BufferedSource = mock()
         source.readString(any()).invoked.thenThrow(SocketTimeoutException())
         val response: Response = Response.Builder()
-                .code(200)
-                .request(Request.Builder().url("https://test.com/").build())
-                .protocol(Protocol.HTTP_1_1)
-                .body(ResponseBody.create(
+            .code(200)
+            .request(Request.Builder().url("https://test.com/").build())
+            .protocol(Protocol.HTTP_1_1)
+            .body(
+                ResponseBody.create(
                     "application/json; charset=utf-8".toMediaTypeOrNull(),
                     1024,
                     source
-                ))
-                .build()
+                )
+            )
+            .build()
         setResponse(client, response)
         return client
     }
