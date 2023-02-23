acd395eace2a10ab38c758a12a986d3013670145
takke <takke30@gmail.com>
2023-02-23 13:21:38 +0900

    add repliesPolicyValue

--- core/src/main/java/mastodon4j/api/entity/MstList.kt
+++ core/src/main/java/mastodon4j/api/entity/MstList.kt
@@ -4,5 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 class MstList(
     @SerializedName("id") val id: Long = 0L,
-    @SerializedName("title") val title: String = ""
+    @SerializedName("title") val title: String = "",
+    @SerializedName("replies_policy") val repliesPolicyValue: String? = MstListRepliesPolicy.List.value,
 )
