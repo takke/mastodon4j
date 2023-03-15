f62c86b2b6d059de399c96f6d07bcd217fc6d90f
takke <takke30@gmail.com>
2023-03-15 10:02:15 +0900

    getBookmarks

--- core/src/main/java/mastodon4j/api/method/MastodonClientEx.kt
+++ core/src/main/java/mastodon4j/api/method/MastodonClientEx.kt
@@ -5,6 +5,7 @@ import mastodon4j.MastodonClient
 val MastodonClient.accounts get() = AccountsMethod(this)
 val MastodonClient.apps get() = AppsMethod(this)
 val MastodonClient.blocks get() = BlocksMethod(this)
+val MastodonClient.bookmarks get() = BookmarksMethod(this)
 val MastodonClient.favourites get() = FavouritesMethod(this)
 val MastodonClient.followRequests get() = FollowRequestsMethod(this)
 val MastodonClient.follows get() = FollowsMethod(this)
