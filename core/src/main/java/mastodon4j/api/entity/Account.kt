10bc2d0f6953e4f4cc22d57145595268c8f13236
takke <takke30@gmail.com>
2023-04-10 13:29:44 +0900

    care calckey.jp

--- core/src/main/java/mastodon4j/api/entity/Account.kt
+++ core/src/main/java/mastodon4j/api/entity/Account.kt
@@ -6,11 +6,11 @@ class Account(
     @SerializedName("id") val id: Long = 0L,
     @SerializedName("username") val userName: String = "",
     @SerializedName("acct") val acct: String = "",
-    @SerializedName("display_name") val displayName: String? = "",  // null when calckey.jp
-    @SerializedName("note") val note: String = "",
+    @SerializedName("display_name") val displayName_: String? = "",  // null when calckey.jp
+    @SerializedName("note") val note_: String? = "",                 // null when calckey.jp
     @SerializedName("url") val url: String = "",
     @SerializedName("avatar") val avatar: String = "",
-    @SerializedName("header") val header: String = "",
+    @SerializedName("header") val header_: String? = "",             // null when calckey.jp
     @SerializedName("locked") val isLocked: Boolean = false,
     @SerializedName("created_at") val createdAt: String = "",
     @SerializedName("followers_count") val followersCount: Long = 0,
@@ -18,4 +18,8 @@ class Account(
     @SerializedName("statuses_count") val statusesCount: Long = 0,
     @SerializedName("emojis") val emojis: List<Emoji> = emptyList(),
     @SerializedName("fields") val fields: List<Field> = emptyList(),
-)
+) {
+    val displayName: String get() = displayName_ ?: ""
+    val note: String get() = note_ ?: ""
+    val header: String get() = header_ ?: ""
+}
