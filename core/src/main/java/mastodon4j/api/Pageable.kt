5f490d99c10a77c8490ca12e625e6499e28b61c9
takke <takke30@gmail.com>
2023-04-13 21:37:19 +0900

    sinceId: String

--- core/src/main/java/mastodon4j/api/Pageable.kt
+++ core/src/main/java/mastodon4j/api/Pageable.kt
@@ -1,8 +1,8 @@
 package mastodon4j.api
 
-class Pageable<T>(val part: List<T>, val link: mastodon4j.api.Link?) : MastodonResponse() {
+class Pageable<T>(val part: List<T>, val link: Link?) : MastodonResponse() {
 
     fun nextRange(limit: Int = 20): Range = Range(link?.maxId, limit = limit)
-    fun prevRange(limit: Int = 20): Range = Range(sinceId = link?.sinceId, limit = limit)
-    fun toRange(limit: Int = 20): Range = Range(link?.maxId, link?.sinceId, limit)
+    fun prevRange(limit: Int = 20): Range = Range(sinceId = link?.sinceId.toString(), limit = limit)
+    fun toRange(limit: Int = 20): Range = Range(link?.maxId, link?.sinceId.toString(), limit)
 }
--- core/src/main/java/mastodon4j/api/Range.kt
+++ core/src/main/java/mastodon4j/api/Range.kt
@@ -4,7 +4,7 @@ import mastodon4j.Parameter
 
 class Range
 @JvmOverloads
-constructor(val maxId: Long? = null, val sinceId: Long? = null, val limit: Int = 20) {
+constructor(val maxId: Long? = null, val sinceId: String? = null, val limit: Int = 20) {
     fun toParameter() = Parameter().apply {
         maxId?.let { append("max_id", it) }
         sinceId?.let { append("since_id", it) }
