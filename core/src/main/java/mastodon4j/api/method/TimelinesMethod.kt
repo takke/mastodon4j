493b0b415d9d881f798e97a0bc069875e16ccccf
takke <takke30@gmail.com>
2023-04-19 20:13:44 +0900

    Conversation

--- core/src/main/java/mastodon4j/api/method/TimelinesMethod.kt
+++ core/src/main/java/mastodon4j/api/method/TimelinesMethod.kt
@@ -4,6 +4,7 @@ import mastodon4j.MastodonClient
 import mastodon4j.MastodonRequest
 import mastodon4j.api.Pageable
 import mastodon4j.api.Range
+import mastodon4j.api.entity.Conversation
 import mastodon4j.api.entity.Status
 import mastodon4j.api.exception.MastodonException
 
@@ -11,7 +12,8 @@ import mastodon4j.api.exception.MastodonException
  * see more https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#timelines
  */
 class TimelinesMethod(private val client: MastodonClient) {
-    //  GET /api/v1/timelines/home
+
+    // GET /api/v1/timelines/home
     @JvmOverloads
     @Throws(MastodonException::class)
     fun getHome(range: Range = Range()): MastodonRequest<Pageable<Status>> {
@@ -21,6 +23,20 @@ class TimelinesMethod(private val client: MastodonClient) {
             },
             {
                 client.getSerializer().fromJson(it, Status::class.java)
+            }
+        ).toPageable()
+    }
+
+    // GET /api/v1/conversations
+    @JvmOverloads
+    @Throws(MastodonException::class)
+    fun getConversations(range: Range = Range()): MastodonRequest<Pageable<Conversation>> {
+        return MastodonRequest<Pageable<Conversation>>(
+            {
+                client.get("/api/v1/conversations", range.toParameter())
+            },
+            {
+                client.getSerializer().fromJson(it, Conversation::class.java)
             }
         ).toPageable()
     }
