a6142745719d4d741c2e1bd530fade78ce370e0c
takke <takke30@gmail.com>
2023-04-20 11:02:02 +0900

    getTrends

--- core/src/main/java/mastodon4j/api/method/TrendsMethod.kt
+++ core/src/main/java/mastodon4j/api/method/TrendsMethod.kt
@@ -34,4 +34,32 @@ class TrendsMethod(private val client: MastodonClient) {
             }
         )
     }
+
+    /**
+     * GET /api/v1/trends
+     *
+     * v3.5.0 or later: use getTrendsTags() instead.
+     */
+    @JvmOverloads
+    @Throws(MastodonException::class)
+    fun getTrends(limit: Int? = null, offset: Int? = null): MastodonRequest<List<Tag>> {
+        return MastodonRequest<List<Tag>>(
+            {
+                client.get(
+                    "/api/v1/trends",
+                    Parameter().apply {
+                        limit?.let {
+                            append("limit", it)
+                        }
+                        offset?.let {
+                            append("offset", it)
+                        }
+                    }
+                )
+            },
+            {
+                client.getSerializer().fromJson(it, Tag::class.java)
+            }
+        )
+    }
 }
