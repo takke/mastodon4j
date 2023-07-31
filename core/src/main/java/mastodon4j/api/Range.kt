5832a43821098aafeb561f37061d7f2cc7ae028e
takke <takke30@gmail.com>
2023-07-31 22:12:31 +0900

    add minId

--- core/src/main/java/mastodon4j/api/Range.kt
+++ core/src/main/java/mastodon4j/api/Range.kt
@@ -4,10 +4,11 @@ import mastodon4j.Parameter
 
 class Range
 @JvmOverloads
-constructor(val maxId: Long? = null, val sinceId: String? = null, val limit: Int = 20) {
+constructor(val maxId: Long? = null, val sinceId: String? = null, val limit: Int = 20, val minId: String? = null) {
     fun toParameter() = Parameter().apply {
         maxId?.let { append("max_id", it) }
         sinceId?.let { append("since_id", it) }
         append("limit", limit)
+        minId?.let { append("min_id", it) }
     }
 }
