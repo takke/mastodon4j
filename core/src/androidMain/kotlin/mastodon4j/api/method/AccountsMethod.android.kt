774b95b99a2d450a92bad6a2160a66f5bfb3f88f
takke <takke30@gmail.com>
2025-07-18 23:03:34 +0900

    KMP

--- core/src/androidMain/kotlin/mastodon4j/api/method/AccountsMethod.android.kt
+++ core/src/androidMain/kotlin/mastodon4j/api/method/AccountsMethod.android.kt
@@ -1,68 +1,69 @@
-//package mastodon4j.api.method
-//
-//import mastodon4j.MastodonRequest
-//import mastodon4j.api.entity.CredentialAccount
-//import okhttp3.MediaType.Companion.toMediaType
-//import okhttp3.MultipartBody
-//import okhttp3.RequestBody.Companion.asRequestBody
-//import java.io.File
-//
-///**
-// * Android固有のAccountsMethod拡張関数実装
-// */
-//
-///**
-// * アバター画像を更新
-// * PATCH /api/v1/accounts/update_credentials
-// */
-//actual fun AccountsMethod.updateAvatar(avatarFile: Any, mimeType: String): MastodonRequest<CredentialAccount> {
-//    require(avatarFile is File) { "avatarFile must be a File instance on Android" }
-//
-//    val client = this.getClient()
-//
-//    return MastodonRequest(
-//        {
-//            client.patch(
-//                "/api/v1/accounts/update_credentials",
-//                MultipartBody.Builder()
-//                    .setType(MultipartBody.FORM)
-//                    .addFormDataPart(
-//                        "avatar", avatarFile.name,
-//                        avatarFile.asRequestBody(mimeType.toMediaType())
-//                    )
-//                    .build()
-//            )
-//        },
-//        {
-//            client.getSerializer().fromJson(it, CredentialAccount::class.java)
-//        }
-//    )
-//}
-//
-///**
-// * ヘッダー画像を更新
-// * PATCH /api/v1/accounts/update_credentials
-// */
-//actual fun AccountsMethod.updateHeader(headerFile: Any, mimeType: String): MastodonRequest<CredentialAccount> {
-//    require(headerFile is File) { "headerFile must be a File instance on Android" }
-//
-//    val client = this.getClient()
-//
-//    return MastodonRequest(
-//        {
-//            client.patch(
-//                "/api/v1/accounts/update_credentials",
-//                MultipartBody.Builder()
-//                    .setType(MultipartBody.FORM)
-//                    .addFormDataPart(
-//                        "header", headerFile.name,
-//                        headerFile.asRequestBody(mimeType.toMediaType())
-//                    )
-//                    .build()
-//            )
-//        },
-//        {
-//            client.getSerializer().fromJson(it, CredentialAccount::class.java)
-//        }
-//    )
-//}
+package mastodon4j.api.method
+
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
+import mastodon4j.MastodonRequest
+import mastodon4j.api.entity.CredentialAccount
+import java.io.File
+
+/**
+ * Android固有のAccountsMethod拡張関数実装
+ */
+
+/**
+ * アバター画像を更新
+ * PATCH /api/v1/accounts/update_credentials
+ */
+actual fun AccountsMethod.updateAvatar(avatarFile: Any, mimeType: String): MastodonRequest<CredentialAccount> {
+    require(avatarFile is File) { "avatarFile must be a File instance on Android" }
+
+    val client = this.getClient()
+
+    return MastodonRequest(
+        {
+            client.getHttpClient().submitFormWithBinaryData(
+                url = "${client.baseUrl}/api/v1/accounts/update_credentials",
+                formData = formData {
+                    append("avatar", avatarFile.readBytes(), Headers.build {
+                        append(HttpHeaders.ContentType, mimeType)
+                        append(HttpHeaders.ContentDisposition, "filename=\"${avatarFile.name}\"")
+                    })
+                }
+            ) {
+                method = HttpMethod.Patch
+            }
+        },
+        { responseText ->
+            client.json.decodeFromString<CredentialAccount>(responseText)
+        }
+    )
+}
+
+/**
+ * ヘッダー画像を更新
+ * PATCH /api/v1/accounts/update_credentials
+ */
+actual fun AccountsMethod.updateHeader(headerFile: Any, mimeType: String): MastodonRequest<CredentialAccount> {
+    require(headerFile is File) { "headerFile must be a File instance on Android" }
+
+    val client = this.getClient()
+
+    return MastodonRequest(
+        {
+            client.getHttpClient().submitFormWithBinaryData(
+                url = "${client.baseUrl}/api/v1/accounts/update_credentials",
+                formData = formData {
+                    append("header", headerFile.readBytes(), Headers.build {
+                        append(HttpHeaders.ContentType, mimeType)
+                        append(HttpHeaders.ContentDisposition, "filename=\"${headerFile.name}\"")
+                    })
+                }
+            ) {
+                method = HttpMethod.Patch
+            }
+        },
+        { responseText ->
+            client.json.decodeFromString<CredentialAccount>(responseText)
+        }
+    )
+}
