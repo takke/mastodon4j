7901c8c975a3bb835d8f5d7e01d1ae2d2f9e50ab
takke <takke30@gmail.com>
2025-09-01 13:24:30 +0900

    KMP

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,6 +9,11 @@ kotlin {
     
     jvm("android")
     
+    // iOS用の設定を追加
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -17,6 +22,12 @@ kotlin {
                 implementation(libs.ktor.serialization.kotlinx.json)
                 implementation(libs.kotlinx.serialization.json)
                 implementation(libs.kotlinx.datetime)
+            }
+        }
+        
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
             }
         }
         
@@ -41,6 +52,29 @@ kotlin {
                 implementation("org.mockito:mockito-core:2.7.22")
             }
         }
+        
+        // iOS用のソースセット
+        val iosMain by creating {
+            dependencies {
+                // iOS用のKtorエンジン
+                implementation(libs.ktor.client.darwin)
+            }
+        }
+        
+        val iosTest by creating {
+            dependencies {
+                // iOS用のテスト依存関係（必要に応じて追加）
+            }
+        }
+        
+        // iOS各ターゲットをiosMainに依存させる
+        val iosX64Main by getting { dependsOn(iosMain) }
+        val iosArm64Main by getting { dependsOn(iosMain) }
+        val iosSimulatorArm64Main by getting { dependsOn(iosMain) }
+        
+        val iosX64Test by getting { dependsOn(iosTest) }
+        val iosArm64Test by getting { dependsOn(iosTest) }
+        val iosSimulatorArm64Test by getting { dependsOn(iosTest) }
     }
 }
 
@@ -53,12 +87,25 @@ publishing {
     publications {
         named<MavenPublication>("android") {
             groupId = "com.sys1yagi"
-            artifactId = "mastodon4j-core"
+            artifactId = "mastodon4j-core-android"
             version = "3.0.0-SNAPSHOT"
 
             pom {
-                name.set("mastodon4j-core")
-                description.set("Mastodon4j Core Library")
+                name.set("mastodon4j-core-android")
+                description.set("Mastodon4j Core Library for Android")
+                url.set("https://github.com/sys1yagi/mastodon4j")
+            }
+        }
+        
+        // iOS用のフレームワーク出力設定
+        create<MavenPublication>("ios") {
+            groupId = "com.sys1yagi"
+            artifactId = "mastodon4j-core-ios"
+            version = "3.0.0-SNAPSHOT"
+
+            pom {
+                name.set("mastodon4j-core-ios")
+                description.set("Mastodon4j Core Library for iOS")
                 url.set("https://github.com/sys1yagi/mastodon4j")
             }
         }
