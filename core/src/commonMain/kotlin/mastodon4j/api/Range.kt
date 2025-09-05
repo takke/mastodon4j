2ed6f169d5be61006d48f65ae5f4e2ceca0d5bce
takke <takke30@gmail.com>
2025-09-05 16:55:47 +0900

    data class Range

--- core/src/commonMain/kotlin/mastodon4j/api/Range.kt
+++ core/src/commonMain/kotlin/mastodon4j/api/Range.kt
@@ -2,8 +2,12 @@ package mastodon4j.api
 
 import mastodon4j.Parameter
 
-class Range
-constructor(val maxId: Long? = null, val sinceId: String? = null, val limit: Int = 20, val minId: String? = null) {
+data class Range(
+    val maxId: Long? = null,
+    val sinceId: String? = null,
+    val limit: Int = 20,
+    val minId: String? = null
+) {
     fun toParameter() = Parameter().apply {
         maxId?.let { append("max_id", it) }
         sinceId?.let { append("since_id", it) }
